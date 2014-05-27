9efb2787828352fcdc3d1f2f6da81942b5d5465f
unknown <ctufaro@VM-DEV-DEV3.mhtny.com>
2014-05-27 11:51:10 -0400

    Exporting VWAP to file

--- ultrafinance/tools/stockVWAP.py
+++ ultrafinance/tools/stockVWAP.py
@@ -25,15 +25,22 @@ class VWAP():
 		
 		start = False
 		
-		cursor = conn.execute("SELECT * FROM %s where symbol = 'AMZN' AND time >= '2014-05-23 09:30:00' AND time < '2014-05-23 09:35:00' order by time" % tableName)
+		cursor = conn.execute("SELECT * FROM %s order by symbol,time " % tableName)		
+		f = open('output/vwap','w')
+		f.write("Symbol,Time,Price,Volume,VP,TotalVP,TotalV,VWAP \n")
 		
 		for row in cursor:	
 		
 			symbol = row[1]
 			time = row[2]
-			price = row[6]
+			price = (float(row[4]) + float(row[5]) + float(row[6])) / 3
 			volume = float(row[7])
 			vp = float(volume)*float(price)
+			
+			if(time.split( )[1] == '09:30:00'):
+				totalVP = 0
+				totalV = 0
+				start = False
 			
 			if start == False:
 				totalVPList.append(vp)
@@ -47,14 +54,14 @@ class VWAP():
 				totalVList.append(totalV)
 				totalVPList.append(totalVP)
 				
-			vwap = totalVP/totalV		
-				
-		
-			print "Symbol:%s Time:%s Price:%s Volume:%s VP:%s TotalVP:%s TotalV:%s VWAP:%s" % (symbol,time,price,volume,vp,totalVP,totalV,vwap)
+			vwap = totalVP/totalV			
+
+			f.write("%s,%s,%s,%s,%s,%s,%s,%s \n" % (symbol,time,price,volume,vp,totalVP,totalV,vwap))
 			
 			count = count + 1
 
-		conn.close()		
+		conn.close()
+		f.close()	
 
 if __name__ == '__main__':
 	vwap = VWAP()
