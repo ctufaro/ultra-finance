06ecf2bbbda20546a2113c1cd88d7324cfa90c6e
unknown <ctufaro@VM-DEV-DEV3.mhtny.com>
2014-06-25 13:05:24 -0400

    Changing run script

--- ultrafinance/tools/run.sh
+++ ultrafinance/tools/run.sh
@@ -1,12 +1,4 @@
-sudo python stockCrawler.py -f /home/pi/Projects/ultra-finance/data/symbols/SPY500.list
-sudo python stockQueries.py
-sudo python /home/pi/Projects/python-emailer/sendEmail.py SPY500
-
-sudo python stockCrawler.py -f /home/pi/Projects/ultra-finance/data/symbols/technology.list
-sudo python stockQueries.py
-sudo python /home/pi/Projects/python-emailer/sendEmail.py Technology
-
-sudo python stockCrawler.py -f /home/pi/Projects/ultra-finance/data/symbols/energy.list
-sudo python stockQueries.py
-sudo python /home/pi/Projects/python-emailer/sendEmail.py Energy
+sudo python stockCrawler.py -f /home/pi/Projects/ultra-finance/data/symbols/penny.list -d penny -t tick -p 15 -c true
+sudo python stockQueries.py -r volume -d penny -t ticks
+sudo python /home/pi/Projects/python-emailer/sendEmail.py Penny
 
