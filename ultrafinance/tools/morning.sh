de1049d62ecb3920b32b89fd5c9e330dc5c9db7a
root <root@raspberrypi.(none)>
2014-06-27 22:26:09 +0000

    Changing Linux scripts

--- ultrafinance/tools/morning.sh
+++ ultrafinance/tools/morning.sh
@@ -1,4 +1,5 @@
-sudo python stockCrawler.py -f /home/pi/Projects/ultra-finance/data/symbols/penny.list -d penny -t tick -p 15 -c true
+cd /home/pi/Projects/ultra-finance/ultrafinance/tools
+sudo python stockCrawler.py -f /home/pi/Projects/ultra-finance/data/symbols/penny.list -d penny -t tick -p 30 -c true
 sudo python stockQueries.py -r volume -d penny -t ticks
 sudo python /home/pi/Projects/python-emailer/sendEmail.py Penny
 
